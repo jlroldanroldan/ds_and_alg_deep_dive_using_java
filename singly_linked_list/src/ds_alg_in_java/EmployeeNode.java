2febd59e2bc90a224a0a39ca04e657cc671ec49f
Jorge Roldan-Roldan <jorgeroldanroldan@gmail.com>
2020-09-06 09:51:15 -0400

    Done up to lecture 47

---
+++ singly_linked_list/src/ds_alg_in_java/EmployeeNode.java
@@ -0,0 +1,4 @@
+package ds_alg_in_java;
+
+public class EmployeeNode {
+}
---
+++ singly_linked_list/src/ds_alg_in_java/Main.java
@@ -0,0 +1,18 @@
+package ds_alg_in_java;
+
+import java.util.Objects;
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee janeJones = new Employee("Jane", "Jones", 123);
+        Employee johnDoe = new Employee("John", "Doe", 4567);
+        Employee maySmith = new Employee("Mary", "Smith", 22);
+        Employee mikeWilson = new Employee("Mike", "Wilson", 3245);
+
+    }
+}
+
+
